8ccab14bf9a455b73219121c0be45eb54e192ab4
Thiago Figueiredo <thfigueiredo9703@gmail.com>
2025-09-22 17:57:38 -0300

    styles changed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-document.querySelector(".password-options").classList.remove("exceed-limit");
-document.querySelector(".password-length").classList.remove("exceed-limit");
+const specialChars = "@!,.#%$".split("");
+const numbers = "1234567890".split("");
+const letters = "abcdefghijklmnopqrstuvwxyz".split("");
+const upperLetters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+
+const passwordLengthInput = document.querySelector(".password-length").value;
+const includeSpecialChars = document.querySelector("#special-characters").value;
+const includeNumbers = document.querySelector("#include-numbers").value;
+
+let password = [];
+
+function getRandomIndex(index) {
+  return Math.floor(Math.random() * index);
+}
+
+for (let i = 0; i < letters.length; i++) {
+  password.push(letters[getRandomIndex(letters.length)]);
+}
