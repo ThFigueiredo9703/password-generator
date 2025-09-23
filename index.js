b3df15dfea0a5afad13c4883e7f41b88653fdcdd
Thiago Figueiredo <thfigueiredo9703@gmail.com>
2025-09-23 16:44:56 -0300

    test

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const specialChars = "@!,.#%$".split("");
 const numbers = "1234567890".split("");
-const letters = "abcdefghijklmnopqrstuvwxyz".split("");
-const upperLetters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split(
+  ""
+);
 const passwordLengthInput = document.querySelector(".password-length").value;
 const includeSpecialChars = document.querySelector("#special-characters").value;
 const includeNumbers = document.querySelector("#include-numbers").value;
 
-let password = [];
+let password = "";
 
-function getRandomIndex(index) {
-  return Math.floor(Math.random() * index);
+function getRandomNumber(num) {
+  return Math.floor(Math.random() * num);
 }
 
-for (let i = 0; i < letters.length; i++) {
-  password.push(letters[getRandomIndex(letters.length)]);
+// Joga os caracteres dentro do array password
+for (let i = 0; i < getRandomNumber(letters.length); i++) {
+  password += letters[getRandomNumber(letters.length)];
 }
